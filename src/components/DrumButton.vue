0e375a081af7b5d3b4cf6c9bb3e4142cec267bab
martin-coutts <martinc@traveltek.net>
2020-06-07 14:54:05 +0100

    Class binding now working correctly as well as repeat of keydown being prevented

--- src/components/DrumButton.vue
+++ src/components/DrumButton.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="drum-button" @click="logClick">
+  <div
+    class="drum-button"
+    :class="{ 'drum-button--selected': this.isKeyPressed }"
+    @mousedown="mouseDown"
+    @mouseup="mouseUp"
+  >
     <div class="drum-button__key">
       <span>{{ sample.key }}</span>
     </div>
@@ -7,17 +12,6 @@
 </template>
 
 <script>
-// const keyDown = (eventKeyCode, sample) => {
-//   if (sample.keyCode === event.keyCode) {
-//     const audio = new Audio(sample.sample);
-//     audio.pause();
-//     audio.play();
-//   }
-// };
-// const keyUp = (event) => {
-//   console.log("keyup");
-// };
-
 export default {
   name: "DrumButton",
   props: ["sample"],
@@ -33,13 +27,15 @@ export default {
   methods: {
     addEventListener(eventType, callback, sample) {
       window.addEventListener(eventType, function(event) {
-        callback(event.keyCode, sample);
+        // *Stops event firing infinite times if key is held down, issue on IE
+        const repeat = event.repeat;
+
+        repeat === false ? callback(event.keyCode, sample) : null;
       });
     },
     keyDown(eventKeyCode, sample) {
       if (sample.keyCode === event.keyCode) {
         this.isKeyPressed = true;
-        console.log("this.isKeyPressed", this.isKeyPressed);
         const audio = new Audio(sample.sample);
         audio.pause();
         audio.play();
@@ -47,12 +43,18 @@ export default {
     },
     keyUp(event) {
       this.isKeyPressed = false;
-      console.log("this.isKeyPressed", this.isKeyPressed);
     },
 
-    logClick(e) {
-      var audio = new Audio(this.sample.sample);
+    mouseDown(e) {
+      console.log("click", e);
+      this.isKeyPressed = true;
+
+      const audio = new Audio(this.sample.sample);
+      audio.pause();
       audio.play();
+    },
+    mouseUp(e) {
+      this.isKeyPressed = false;
     },
   },
 };
@@ -63,6 +65,11 @@ export default {
 .drum-button {
   height: 200px;
   width: 200px;
+  background-color: $button-black;
   border: solid 10px $button-border-grey;
+
+  &--selected {
+    border: solid 10px $button-border-selected-red;
+  }
 }
 </style>
